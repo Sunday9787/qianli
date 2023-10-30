d698b7e5105cc5d6deafa25b72298d15d9c46c21
Edward <809537981@qq.com>
2023-10-30 16:28:53 +0800

    feat: layout header 添加 搜索栏

--- services/frontend/src/js/layout.ts
+++ services/frontend/src/js/layout.ts
@@ -32,3 +32,18 @@ log(process.env.VERSION, 'version')
 $('.qianli-footer-backtop').on('click', function () {
   $('body,html').animate({ scrollTop: 0 }, 800)
 })
+
+$('.qianli-header-form').on('click', '.qianli-header-form__icon.icon-search', function (e) {
+  $('.qianli-header-form-inner').toggleClass('open')
+  $('.qianli-header-form__icon.icon-close').show()
+  $(e.currentTarget).toggle()
+})
+
+$('.qianli-header-form').on('click', '.qianli-header-form__icon.icon-close', function (e) {
+  $('.qianli-header-form-inner').toggleClass('open')
+  $('.qianli-header-form__icon.icon-search').show()
+  $(e.currentTarget).toggle()
+
+  const form = e.delegateTarget as HTMLFormElement
+  form.reset()
+})
