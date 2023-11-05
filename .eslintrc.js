26f09b3c2c43999f57a7947306774f567decb2aa
Edward <809537981@qq.com>
2023-11-05 12:39:15 +0800

    ci: 调整 公共配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: ['./services/backend/tsconfig.json', './services/frontend/tsconfig.json'],
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
