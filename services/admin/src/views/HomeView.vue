547274a568869f041a8265114d6067fd6aaa0434
Edward <809537981@qq.com>
2023-11-06 17:37:40 +0800

    feat[workspace]: 初始化 admin

---
+++ services/admin/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template lang="pug">
+main
+  TheWelcome
+</template>
---
+++ services/admin/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.vue', './src/**/*.tsx'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
